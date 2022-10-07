8b6a93340e49f0bb02a743410afcdd467913084c
Narbok <narbok29@gmail.com>
2022-10-07 14:36:13 -0500

    update

--- src/app/components/pagina1/pagina1.component.ts
+++ src/app/components/pagina1/pagina1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CardComponent } from '../card/card.component';
 import { HttpClient } from '@angular/common/http';
@@ -8,44 +8,59 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './pagina1.component.html',
   styleUrls: ['./pagina1.component.scss']
 })
-export class Pagina1Component implements OnInit{
-  
-  public apiMovie!: any;
+export class Pagina1Component implements OnInit {
+
+  public apiMovie: any[]=[];
+  public page: number = 1;
 
   constructor(public dialog: MatDialog, private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getData()
+    this.getData(this.page)
   }
 
   openDialog(id: number) {
-    console.log('open > ',id);
-    this.dialog.open(CardComponent,{
+    console.log('open > ', id);
+    this.dialog.open(CardComponent, {
       data: id
     });
   }
-  
-  getData() {
-    const user = 'https://api.themoviedb.org/3/discover/movie?api_key=ee0e78a4218de29740513ad6fc39e3b8&language=es-MX&region=MX&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1&with_genres=28&with_watch_monetization_types=buy';
-    let response = this.http.get(user).subscribe((resp:any) => {
-      this.apiMovie = resp['results']
-      return resp;
+
+  getData(pageURL:number) {
+    const user = `https://api.themoviedb.org/3/discover/movie?api_key=ee0e78a4218de29740513ad6fc39e3b8&with_genres=878&page=${pageURL}`
+    let response = this.http.get(user).subscribe((resp: any) => {
+      this.apiMovie = this.apiMovie.concat(resp.results)  // concat()
     })
   }
-}
-  // -----------------------------------  map christian
-  // getMovies(): Observable<any>{
-  //   return this.http.get(this.apiMovie).pipe(
-  //     map((res:any) => {
-  //       const response: any [] =[]
-  //       res.results.forEach((el:any) => {
-  //         let newRes = {
-  //           id: el.id,
-  //           poster: el.poster_path
-  //         }
-  //         response.push(newRes)
-  //       });
-  //       return response
-  //     })
-  //   )
+
+  @HostListener("window:scroll")
+  onScroll(): void {
+    if (!this.bottomReached()) {
+      console.log('entro');
+      this.getData(this.page++)
+    }
+  }
+
+  bottomReached(): boolean {
+    return (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+  }
+
+  // onScroll(event: any){
+  //   let _target = Element = event.target;
+  //   console.log('>',_target);
+  //   let position = _target.scrollTop;
+  //   console.log('>>',position);
+  //   let scrollH = _target.scrollHeigth; 
+  //   console.log('>>>',scrollH);
+  //   let element = _target.cleintHeigth;
+  //   console.log('>>>>',element);
+
+  //   let maxPosition = scrollH - element - 300;
+  //   console.log('>=',maxPosition);
+
+  //   if (position >= maxPosition) {
+  //     this.page++
+  //     this.getData(this.page);
+  //   }
   // }
+}
--- src/app/components/pagina4/pagina4.component.ts
+++ src/app/components/pagina4/pagina4.component.ts
@@ -26,7 +26,7 @@ export class Pagina4Component implements OnInit{
   }
   
   getData() {
-    const user = 'https://api.themoviedb.org/3/discover/movie?api_key=ee0e78a4218de29740513ad6fc39e3b8&language=es-MX&region=MX&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1&with_genres=35&with_watch_monetization_types=buy';
+    const user = 'https://api.themoviedb.org/3/discover/movie?api_key=ee0e78a4218de29740513ad6fc39e3b8&with_genres=35';
     let response = this.http.get(user).subscribe((resp:any) => {
       this.apiMovie = resp['results']
       return resp;
